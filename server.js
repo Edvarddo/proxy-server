22232b67e9d25b523db169c005457736f4e203cc
edv <eduardo.bautista@inacapmail.cl>
2024-11-02 21:17:30 -0300

    añadir renovación token

--- server.js
+++ server.js
@@ -3,18 +3,72 @@ const axios = require('axios');
 const cors = require('cors'); // Importa el paquete cors
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-
-
 const PORT = process.env.PORT || 8080;
-const auth_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMwMjU4MjY1LCJpYXQiOjE3MzAxNzE4NjUsImp0aSI6IjYxYjNiN2ExNTFmMTRkZDc4YTc0MjE4NjMwMjRkZTcxIiwicnV0IjoiMjAxMjM5MzAtNSJ9.10FaSb1UjuAgSCJOTF7sfS3Dzec4v06QWVZ7zNhlLmk"
+let auth_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMwNjc3NDU2LCJpYXQiOjE3MzA1OTEwNTYsImp0aSI6IjkxYzRjN2UwNGE4ZjQ5NGVhYzkyZTAzMzMyYmJlY2JiIiwicnV0IjoiMjAxMjM5MzAtNSJ9.BwPEOrK4ronwERUKM5PpJ4qHeUQld2bRMTTRsTIR-74"
+
+function getToken() {
+  const url =  `${originalUrl}/token/`;
+  console.log(url);
+  axios.post( url, {
+    "rut": "20123930-5",
+    "password": "@d3Vpr0Tot1no"
+  })
+    .then(function (response) {
+      // console.log(response.data.access);
+      console.log("token saved");
+      auth_token = response.data.access;
+    }
+    )
+    .catch(function (error) {
+      console.log(error);
+    }
+    );
+}
+// every 10 seconds for testing
+// for 
+// setInterval(getToken, 1000 * 10); // Cada 10 segundos
+setInterval(getToken, 1000 * 60 * 60 * 24); // Cada 24 horas
+
+
+
+
+
 app.use(express.json());
 app.use(cors(
   {
     origin: '*'
   }
 ));
-const originalUrl = "http://3.217.85.102/api/v1"
 
+const originalUrl = "http://3.217.85.102/api/v1"
+// getToken();
+
+// get token and save endpoint
+app.get('/api-proxy/token/', async (req, res) => {
+  const url = `${originalUrl}/token/`;
+  console.log(url);
+  try {
+    const response = await axios.post(url, {
+      "rut": "20123930-5",
+      "password": "@d3Vpr0Tot1no"
+    }).
+      then(function (response) {
+        console.log("prev_tok: ",auth_token);
+        auth_token = response.data.access ? response.data.access : auth_token;
+        console.log("actual_tok: ",response.data.access);
+        res.json(response.data);
+      }
+      )
+      .catch(function (error) {
+        console.log(error);
+      }
+      );
+    
+     
+  } catch (error) {
+    res.status(500).send("Error en el proxy");
+  }
+});
 
 // Configurar el proxy endpoint
 app.get('/api-proxy/publicaciones/', async (req, res) => {
@@ -94,6 +148,6 @@ app.get('/api-proxy/export-to-excel/', async (req, res) => {
 
 
 
-app.listen(PORT, HOST = "192.168.0.10", () => {
+app.listen(PORT, HOST = "192.168.0.11", () => {
   console.log(`Proxy server en el puerto ${PORT}`);
 });
